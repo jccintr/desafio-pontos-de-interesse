bbf5e7bff485f3c3bcce3862bfe51cf85b13b2b6
Julio Cesar <jccintr@gmail.com>
2025-05-17 01:51:07 -0300

    Projeto criado

---
+++ src/main/java/com/jcsoftware/desafio_pontos/DesafioPontosApplication.java
@@ -0,0 +1,13 @@
+package com.jcsoftware.desafio_pontos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPontosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPontosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcsoftware/desafio_pontos/DesafioPontosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcsoftware.desafio_pontos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPontosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
